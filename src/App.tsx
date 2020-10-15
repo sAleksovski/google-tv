b15a82e3111bcdc4baf72d76e70e1bacba86a47f
Stefan Aleksovski <aleksovski.stefan994@gmail.com>
2020-10-15 23:00:37 +0200

    feat: add initial dependencies and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { BaseStyles, NormalizeStyles } from './styles';
+import './styles/fontStyles.css';
+
+const StyledText = styled.div`
+  font-weight: bold;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NormalizeStyles />
+      <BaseStyles />
+      <div>
+        <StyledText>Hello</StyledText>
+      </div>
+    </>
   );
 }
 
---
+++ src/styles/BaseStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  /* Reset box-sizing */
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #f5f5f5;
+  }
+`;
